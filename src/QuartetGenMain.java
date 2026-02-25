ad715eac2f9c309dc400dcbae06d0d43cae6d454
abdur-rafi <abdurrafi403@gmail.com>
2026-02-25 22:29:17 +0600

    Integrated quartet decomposition and wqfm 2020

---
+++ src/QuartetGenMain.java
@@ -0,0 +1,24 @@
+package src;
+
+import src.PreProcessing.GeneTrees;
+import src.Quartets.QuartestsList;
+
+public class QuartetGenMain {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("Usage: java -cp <classpath> src.QuartetGenMain <gene-trees-file>");
+            System.exit(1);
+        }
+
+        String path = args[0];
+
+        GeneTrees geneTrees = new GeneTrees(path);
+        geneTrees.readTaxaNames();
+
+        QuartestsList qlist = geneTrees.readGeneTreesAndGenerateQuartets();
+
+        qlist.printQuartets(geneTrees.taxa);
+
+    }
+}
